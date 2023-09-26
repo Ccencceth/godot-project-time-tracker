95cc434a71d40cb25a918bdf773058eb471da4d5
Ccencceth <45124495+Ccencceth@users.noreply.github.com>
2023-09-27 06:40:32 +0800

    it works

--- addons/project_time_tracker/project_timer.gd
+++ addons/project_time_tracker/project_timer.gd
@@ -2,10 +2,13 @@
 extends Control
 
 
+@export var total_time_label_path : NodePath
+@export var session_time_label_path : NodePath
 var session_time = 0
 var total_time = 0
-var save : ConfigFile = ConfigFile.new()
-const PATH_TO_SAVE_FILE = "user://time_in_project.ini"
+const PATH_TO_SAVE_FILE = "user://time_in_project.save"
+@onready var total_time_label : Label = get_node(total_time_label_path)
+@onready var session_time_label : Label = get_node(session_time_label_path)
 
 
 func _enter_tree() -> void:
@@ -17,15 +20,33 @@ func _exit_tree() -> void:
 
 
 func _process(delta: float) -> void:
-	pass
+	session_time += delta
+	total_time += delta
+	
+	set_time()
+
+
+func set_time():
+	total_time_label.text = "Time in project: " + Time.get_time_string_from_unix_time(total_time)
+	session_time_label.text = "Time this session: " + Time.get_time_string_from_unix_time(session_time)
 
 
 func read_file():
 	if not FileAccess.file_exists(PATH_TO_SAVE_FILE):
 		return
 	
-	var file = FileAccess.open(PATH_TO_SAVE_FILE, FileAccess.READ)
+	var save = FileAccess.open(PATH_TO_SAVE_FILE, FileAccess.READ)
+	
+	var json_string = save.get_line()
+	
+	var parsed_result = JSON.parse_string(json_string)
+	
+	print(parsed_result)
+	
+	total_time = parsed_result['time']
+	
 
 
 func save_file():
-	pass
+	var save = FileAccess.open(PATH_TO_SAVE_FILE, FileAccess.WRITE)
+	save.store_line(JSON.stringify({'time': total_time}))
