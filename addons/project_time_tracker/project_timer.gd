001d7ed7797d5d5112fced033f3706ffead86dce
Ccencceth <45124495+Ccencceth@users.noreply.github.com>
2023-09-27 10:41:39 +0800

    added ability to set the time manually

--- addons/project_time_tracker/project_timer.gd
+++ addons/project_time_tracker/project_timer.gd
@@ -2,13 +2,12 @@
 extends Control
 
 
-@export var total_time_label_path : NodePath
-@export var session_time_label_path : NodePath
 var session_time = 0.0
 var total_time = 0.0
 const PATH_TO_SAVE_FILE = "user://time_in_project.save"
-@onready var total_time_label : Label = get_node(total_time_label_path)
-@onready var session_time_label : Label = get_node(session_time_label_path)
+@onready var total_time_label : Label = get_node("VBoxContainer/TimeInProject")
+@onready var session_time_label : Label = get_node("VBoxContainer/TimeThisSession")
+@onready var settings_window : Window = get_node("SettingsWindow")
 
 
 func _enter_tree() -> void:
@@ -26,12 +25,36 @@ func _process(delta: float) -> void:
 	set_time()
 
 
-func set_time():
-	total_time_label.text = "Time in project: " + Time.get_time_string_from_unix_time(total_time)
-	session_time_label.text = "Time this session: " + Time.get_time_string_from_unix_time(session_time)
+func set_time() -> void:
+	total_time_label.text = "Time in project: " + get_time_string_from_unix(total_time)
+	session_time_label.text = "Time this session: " + get_time_string_from_unix(session_time)
 
 
-func read_file():
+func get_time_string_from_unix(unix_time):
+	var time_string = ""
+	var unix_time_int = int(unix_time)
+	
+	var hours = floor(unix_time_int / 3600)
+	if hours < 10:
+		time_string += "0"
+	time_string += str(hours) + ":"
+	unix_time_int -= hours * 3600
+	
+	var minutes = floor(unix_time_int / 60)
+	if minutes < 10:
+		time_string += "0"
+	time_string += str(minutes) + ":"
+	unix_time_int -= minutes * 60
+	
+	var seconds = unix_time_int
+	if seconds < 10:
+		time_string += "0"
+	time_string += str(seconds)
+	
+	return time_string
+
+
+func read_file() -> void:
 	if not FileAccess.file_exists(PATH_TO_SAVE_FILE):
 		return
 	
@@ -47,6 +70,20 @@ func read_file():
 	
 
 
-func save_file():
+func save_file() -> void:
 	var save = FileAccess.open(PATH_TO_SAVE_FILE, FileAccess.WRITE)
 	save.store_line(JSON.stringify({'time': roundf(total_time)}, "\t"))
+
+
+func _on_settings_button_pressed() -> void:
+	settings_window.show()
+
+
+func _on_settings_window_close_requested() -> void:
+	settings_window.hide()
+
+
+func _on_settings_window_set_new_time(hours, minutes, seconds) -> void:
+	var unix_time = hours * 3600 + minutes * 60 + seconds
+	total_time = unix_time
+	print("Set Time in project to: " + get_time_string_from_unix(total_time))
